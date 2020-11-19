09ad16115868d5de0e65fc304cb0ba169af48c67
venkat-rytle <venkat.prabhu@rytle.com>
2020-11-19 15:30:53 +0530

    Wifi info

--- Welcome Swift/Welcome Swift/ViewController.swift
+++ Welcome Swift/Welcome Swift/ViewController.swift
@@ -6,22 +6,102 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+import SystemConfiguration.CaptiveNetwork
 
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
     @IBOutlet weak var lbl: UILabel!
+    
+    var locationManager = CLLocationManager()
+
+    
+    var ssid = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+     
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        // 1
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            return
+            
+        // 2
+        case .denied, .restricted:
+            let alert = UIAlertController(title: "Location Services disabled", message: "Please enable Location Services in Settings", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            
+            present(alert, animated: true, completion: nil)
+            return
+        case .authorizedAlways, .authorizedWhenInUse:
+            break
+            
+        }
+        
+        // 4
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
         
         
-        say("Hello Universe")
+      
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
 
+        let ssid = self.getConnectedWifiInfo()
+
+            print(ssid?["SSID"])
+            
+        }
+        
+    }
+    override func viewWillLayoutSubviews() {
+        
+        lbl.font = .systemFont(ofSize: 14)
+        lbl.text = "HI"
+        lbl.sizeToFit()
+    }
+    
     func say(_ text: String) {
         
         print(text)
+    }
+}
+
+extension ViewController {
+    
+    // 1
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let currentLocation = locations.last {
+            print("Current location: \(currentLocation)")
+        }
+    }
+
+    // 2
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func getConnectedWifiInfo() -> [AnyHashable: Any]? {
+
+        if let ifs = CFBridgingRetain( CNCopySupportedInterfaces()) as? [String],
+            let ifName = ifs.first as CFString?,
+            let info = CFBridgingRetain( CNCopyCurrentNetworkInfo((ifName))) as? [AnyHashable: Any] {
+
+            return info
+        }
+        return nil
+
     }
 }
 
